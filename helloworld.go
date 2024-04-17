9b5489af2e09109492e2b90b5aeefef5731f21b9
gaorunboa <73617970+gaorunboa@users.noreply.github.com>
2024-04-17 10:30:29 +0800

    Add files via upload

---
+++ helloworld.go
@@ -0,0 +1,8 @@
+﻿package helloworld  
+  
+import "fmt"  
+  
+// PrintHelloWorld prints "Hello, World!" from the helloworld package.  
+func PrintHelloWorld() {  
+    fmt.Println("Hello, World! From the helloworld package.")  
+}
