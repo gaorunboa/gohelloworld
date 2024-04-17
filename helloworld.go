7e197d36bb1c6bc00e6cb804331358a39bbf738e
gaorunboa <73617970+gaorunboa@users.noreply.github.com>
2024-04-17 10:57:17 +0800

    Update helloworld.go

--- helloworld.go
+++ helloworld.go
@@ -4,5 +4,5 @@ import "fmt"
   
 // PrintHelloWorld prints "Hello, World!" from the helloworld package.  
 func PrintHelloWorld() {  
-    fmt.Println("Hello, World! From the helloworld package.")  
+    fmt.Println("Hello, World 2! From the helloworld package.")  
 }
